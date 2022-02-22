2b90e9ebb411fb278d9a8ddc14f7497885349a52
Tolulope Dada <tdada0912@gmail.com>
2022-02-22 18:43:22 +0100

    Updated the program to catch errors made by the user

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# Initialization of variables
 numUnits = [5.0, 4.0, 3.0, 2.0, 1.0, 0.0]
 numOfCourses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+totalNumOfSemesters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 qualityPoints = []
 summation = 0
 totalUnits = 0
@@ -9,12 +11,24 @@ cSummation = 0
 cTotalUnits = 0
 cGpa = 0
 
+# program to get the number of semesters completed by the user and check to see that the input is valid
+
 getStudentsSemester = eval(input("Enter the number of semesters completed: "))
+if getStudentsSemester not in totalNumOfSemesters:
+    raise Exception("Sorry, Invalid Input")
+
+# program to obtain the number of courses offered by the user and to check to see that the input is valid
+
 for semesterCompleted in range(0, getStudentsSemester):
     getNumberOfCourse = eval(input("How many courses are you offering for this semester: "))
+    if getNumberOfCourse not in numOfCourses:
+        raise Exception("Sorry, Invalid Input")
+
+# program to calculate the total quality points and total units
+
     if getNumberOfCourse in numOfCourses:
         for num in range(0, getNumberOfCourse):
-            grade = int(input("Enter your grade: "))
+            grade = int(input("Enter your score: "))
             unitOfCourse = int(input("Enter the unit of this course: "))
 
             if 70 <= grade <= 100:
@@ -35,19 +49,33 @@ for semesterCompleted in range(0, getStudentsSemester):
 
             totalUnits = totalUnits + unitOfCourse
 
+            if totalUnits == 0:
+                raise Exception("Sorry, you cannot have 0 units in a course")
+
+# program to calculate one semester gpa
+
     if getStudentsSemester == 1:
         for i in qualityPoints:
             summation = summation + i
 
         gpa = summation / totalUnits
-
         print(f"Your semester's gpa is {gpa}")
 
     print("\n")
 
+# program to calculate the cgpa based on the number of semesters obtained from the user
+
 if getStudentsSemester > 1:
     cTotalUnits += totalUnits
+    print(cTotalUnits)
     for i in qualityPoints:
         cSummation += i
+    print(cSummation)
     cGpa = cSummation / cTotalUnits
+
+    try:
+        print(f"Your semester's gpa is {gpa}")
+    except ZeroDivisionError:
+        print("Sorry, you can't divide by 0")
+
     print(f"Your CGPA is {cGpa}")
